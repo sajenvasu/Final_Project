57920421fe1728e135a779d264a25c0833ea7cfc
sameeksha-nair <sameeksha.canada@gmail.com>
2023-07-06 15:01:55 -0400

    first front page update
    base line of the front page

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function scrolltoLogin() {
+  const section = document.getElementById("logintab");
+  window.scrollTo({
+    top: section.offsetTop,
+    behavior: "smooth"
+  });
+
+}
+
+  function scrolltoGames() {
+    const section = document.getElementById("gamestab");
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: "smooth"
+    });
+  
+}
