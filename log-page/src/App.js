626169ee66b3a515ae62608ff0b5af559b48ca68
Sajen Vasuthevan <vasuthe@uwindsor.ca>
2023-07-24 14:22:27 -0400

    Backend Server and Database Established

--- log-page/src/App.js
+++ log-page/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
+// import {BrowserRouter as Route, Router, Routes} from "react-router-dom";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <h1>App</h1>
       <Login/>
       <Signup/>
+      <Home/>
     </div>
   );
 }
---
+++ log-page/src/components/Home.js
@@ -0,0 +1,6 @@
+function Home(){
+    return(
+        <h2>This is the homepage</h2>
+    );
+}
+export default Home;
