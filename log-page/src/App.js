3dd2a3f5fd1715f0b418b4f8a5b572822b7e078e
Sajen Vasuthevan <vasuthe@uwindsor.ca>
2023-07-24 16:52:27 -0400

    Server Side is up

--- log-page/src/App.js
+++ log-page/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
-// import {BrowserRouter as Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter as Route, Router, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>App</h1>
-      <Login/>
-      <Signup/>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route element={<Login/>} path="/login"/>
+          <Route element={<Signup/>} path="/signup"/>
+          <Route element={<Home/>} path="/"/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- log-page/src/components/Login.js
+++ log-page/src/components/Login.js
@@ -1,6 +1,33 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
 function Login(){
+
+ 
+    function clickedlogin(){
+        console.log("Clicked")
+    }
+
     return(
-        <h2>Login</h2>
+        <div className="login_div">
+            <h1>Login</h1>
+            <form action = "POST">
+                <label>Gamer ID:  
+                    <input type = "text" id="gamer_TextBox" className="log_TB"/>
+                </label>
+                <br/>
+                <label>Password:  
+                    <input type = "text" id="password_TextBox" className="log_TB"/>
+                </label>
+                <br/>
+                <input type = "Submit" onClick = {clickedlogin}/>
+            </form>
+             
+            <div>
+            <Link to = "/">Continue as Guest</Link>
+            </div>
+        </div>
     );
 }
 export default Login;
