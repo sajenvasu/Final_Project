a2630a3f67c6eb7708e112e19de0b26dbe85e594
sameeksha-nair <sameeksha.canada@gmail.com>
2023-07-26 17:38:28 -0400

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,95 @@
+/*const mongoose = require('mongoose');
+const url = "mongodb://127.0.0.1:27017/Quiz";
+
+mongoose.connect(url).then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});*/
+
+
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require("cors")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const UserModel = require('./models/User')
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+
+const app = express()
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+app.use(cookieParser())
+app.use(cors(corsOptions));
+
+mongoose.connect('mongodb://127.0.0.1:27017/employee');
+
+const verifyLogin = (req, res, next) => {
+    const token = req.cookies.token;
+    if(!token) {
+        return res.json("Token is missing")
+    } else {
+        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+            if(err) {
+                return res.json("Error with token")
+            } else {
+                if(decoded.role === "admin") {
+                    next()
+                } else {
+                    return res.json("not admin")
+                }
+            }
+        })
+    }
+}
+
+app.get('/dashboard',verifyLogin ,(req, res) => {
+    res.json("Success")
+})
+
+app.post('/Create', (req, res) => {
+    const {name, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        UserModel.create({name, email, password: hash})
+        .then(user => res.json("Success"))
+        .catch(err => res.json(err))
+    }).catch(err => res.json(err))
+})
+
+app.post('/Login', (req, res) => {
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            bcrypt.compare(password, user.password, (err, response) => {
+                if(response) {
+                  const token = jwt.sign({email: user.email, role: user.role},
+                        "jwt-secret-key", {expiresIn: '1d'})  
+                    res.cookie('token', token)
+                    return res.json({Status: "Success", role: user.role})
+                }else {
+                    return res.json("The password is incorrect")
+                }
+            })
+        } else {
+            return res.json("No record existed")
+        }
+    })
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
