1d5d28d2faa842f8b98bf108ec73ac68990ea2a3
Sajen Vasuthevan <vasuthe@uwindsor.ca>
2023-07-28 00:46:47 -0400

    Working quiz

--- samtest/src/game3/beginquiz.js
+++ samtest/src/game3/beginquiz.js
@@ -3,9 +3,9 @@ import './quiz-style.css';
 function beginquiz(){
     return(
         <div className = "beginQuizCont">
-            <h3>Begin Quiz</h3>
-            <h5>After learning about cybersecurity threats<br/> take this quiz to test your knowledge!</h5>
-            <Link to= "/quiz"><button className="quizbutton">Begin!</button></Link>
+            <h3>Quiz</h3>
+            <h5>After learning about cybersecurity threats take <br/>this quiz to test your knowledge!</h5>
+            <Link to= "/quiz"><button id="beginquizbutton">Begin!</button></Link>
         </div>
     );
 }
--- samtest/src/game3/quiz.js
+++ samtest/src/game3/quiz.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import React, {useState } from 'react';
 import './quiz-style.css';
 function Quiz(){
-
+    
+    const Navigate = useNavigate();
     const [questionCount, setQuestionCount] = useState(0);
-    const [correctAnsOption, setCorrectAnsOption] = useState(0);
-    const [choiceColor, setChoiceColor] = useState();
+    const [prevBtnVal, setPrevBtnVal] = useState(true);
+    const [nextBtnVal, setNextBtnVal] = useState("Next");
+    const [choice1Color, setChoice1Color] = useState("black");
+    const [choice2Color, setChoice2Color] = useState("black");
+    const [choice3Color, setChoice3Color] = useState("black");
+    const [choice4Color, setChoice4Color] = useState("black");
+    var choiceCount = 0;
 
     const quesArr = [
         {
@@ -15,34 +22,99 @@ function Quiz(){
                 {choice: "7"},
                 {choice: "5"},
             ],
-            correctAnsChoice: "4"
+            correctAnsChoice: "4",
         },
-
+        {
+            questions: "5+1",
+            answerChoices: [
+                {choice: "6"},
+                {choice: "4"},
+                {choice: "7"},
+                {choice: "5"},
+            ],
+            correctAnsChoice: "1",
+        },
+        {
+            questions: "9+0",
+            answerChoices: [
+                {choice: "4"},
+                {choice: "1"},
+                {choice: "9"},
+                {choice: "3"},
+            ],
+            correctAnsChoice: "3",
+        },
+        {
+            questions: "3-1",
+            answerChoices: [
+                {choice: "5"},
+                {choice: "2"},
+                {choice: "1"},
+                {choice: "8"},
+            ],
+            correctAnsChoice: "2",
+        },
+        {
+            questions: "4x4",
+            answerChoices: [
+                {choice: "10"},
+                {choice: "12"},
+                {choice: "16"},
+                {choice: "18"},
+            ],
+            correctAnsChoice: "3",
+        },
     ]
 
 
 
     const clickedchoice = (selectedChoice) => {
-        setCorrectAnsOption(quesArr[questionCount].correctAnsChoice);
-        if (selectedChoice == correctAnsOption){
+        if (selectedChoice === quesArr[questionCount].correctAnsChoice){
             console.log("Correct");
         }else{
             console.log("Incorrect");
         }
     }
 
+    const prevques = () => {
+        setQuestionCount(questionCount - 1);
+        if (questionCount === 1){
+            setPrevBtnVal(true);
+        }else if (questionCount <= 4){
+            setNextBtnVal("Next");
+        }
+    }
+
+    const nextques = () => {
+        setQuestionCount(questionCount + 1);
+        if (questionCount === 0){
+            setPrevBtnVal(false);
+        }else if (questionCount === 1 ){
+            setNextBtnVal("Next");
+        }else if (questionCount === 2){
+            setNextBtnVal("Next");
+        }else if (questionCount === 3){
+            setNextBtnVal("Finish Quiz");
+        }else if (questionCount === 4){
+            Navigate("/scores");
+        }
+    }
+
+    const chosenchoice = (x) => {
+
+    }
 
     return(
         <div className = "beginQuizCont">
-            <h3>{quesArr[questionCount].questions}</h3>
+            <h3>{questionCount + 1}. {quesArr[questionCount].questions}</h3>
             <ul>
-                <li id = "mcqch1" onClick={() => clickedchoice("1")}>(A) {quesArr[questionCount].answerChoices[questionCount].choice}</li>
-                <li id = "mcqch2" onClick={() => clickedchoice("2")}>(B) {quesArr[questionCount].answerChoices[questionCount + 1].choice}</li>
-                <li id = "mcqch3" onClick={() => clickedchoice("3")}>(C) {quesArr[questionCount].answerChoices[questionCount + 2].choice}</li>
-                <li id = "mcqch4" onClick={() => clickedchoice("4")}>(D) {quesArr[questionCount].answerChoices[questionCount + 3].choice}</li>
+                <li id = "mcqch1" style={{backgroundColor: choice1Color}} onClick={() => clickedchoice("1")}>(A) {quesArr[questionCount].answerChoices[choiceCount + 0].choice}</li>
+                <li id = "mcqch2" style={{backgroundColor: {choice2Color}}} onClick={() => clickedchoice("2")}>(B) {quesArr[questionCount].answerChoices[choiceCount + 1].choice}</li>
+                <li id = "mcqch3" style={{backgroundColor: {choice3Color}}} onClick={() => clickedchoice("3")}>(C) {quesArr[questionCount].answerChoices[choiceCount + 2].choice}</li>
+                <li id = "mcqch4" style={{backgroundColor: {choice4Color}}} onClick={() => clickedchoice("4")}>(D) {quesArr[questionCount].answerChoices[choiceCount + 3].choice}</li>
             </ul>   
-            <button>Previous Question</button>
-            <button>Next Question</button>
+            <button disabled = {prevBtnVal} onClick={() => prevques()}>Back</button>
+            <button onClick={() => nextques()}>{nextBtnVal}</button>
         </div>
     );
 }
